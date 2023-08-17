0a54dbef28f71b47c95e727148241f60686861cd
Jamie Bernardi <jamie@Jamies-MacBook-Pro-2.local>
2023-08-17 01:12:49 +0100

    Unset clean by default

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ test_qas = [(x, eval(x)) for x in test_sums]
 if __name__ == "__main__":
 
     CLEAN_DATA_LOAD = False
-    CLEAN_ANSWERS = True
+    CLEAN_ANSWERS = False
     EXPERIMENT_RANGE = (0, 5)
 
     data_handler = DataHandler(os.path.join(os.getcwd(), "data"), clean=CLEAN_DATA_LOAD)
