b85171bc44a29c20bf12f497bbe47a4f0008c759
Jamie Bernardi <jamie@Jamies-MacBook-Pro-2.local>
2023-08-23 18:48:49 +0100

    deleting old image data and adding script to view some results

---
+++ view_results.py
@@ -0,0 +1,27 @@
+import pickle
+
+if __name__ == "__main__":
+
+    DATA_DIR = "local_multi_filter_image"
+    IDCS = range(0, 20)
+
+    with open(f"{DATA_DIR}/data.p", "rb") as f:
+        all_data = pickle.load(f)
+
+    for i in IDCS:
+
+        data = all_data[i]
+        if data is None:
+            print(f"Data {i} required image")
+            continue
+        print("\n***********")
+        print(f"QUESTION {i}")
+        print(data["question"])
+        print("\nFIRST")
+        print(data["raw_model_first_answer_0.5"])
+        print(f"->{data['model_first_answer_0.5']}")
+        if data['model_first_answer_0.5'] is None:
+            continue
+        print("\nSECOND")
+        print(data["raw_model_second_answer_0.5"])
+        print(f"->{data['model_second_answer_0.5']}")
